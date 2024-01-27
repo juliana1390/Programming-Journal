c00f15a19c99e60ba8c2ab5de591555daa1dd241
juliana1390 <juliana13290@gmail.com>
2024-01-27 16:16:27 -0300

    get_entries function update (select from table) / row_factory

--- database.py
+++ database.py
@@ -1,5 +1,6 @@
 import sqlite3
 connection = sqlite3.connect("data.db")
+connection.row_factory = sqlite3.Row    # 'dictionary cursor' - instead of tuples it returns 'dictionary' rows 
 
 def create_table():
     with connection:
@@ -15,4 +16,5 @@ def add_entry(entry_content, entry_date):
 
 
 def get_entries():
-    return entries
+    cursor = connection.execute("SELECT * FROM entries;")
+    return cursor
