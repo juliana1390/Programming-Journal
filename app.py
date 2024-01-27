a0bf5b52a9b5d3872743e1d3f3d8907c32e705b2
juliana1390 <juliana13290@gmail.com>
2024-01-27 13:19:02 -0300

    inputs organization

--- app.py
+++ app.py
@@ -8,26 +8,25 @@ menu = """Please select one of the following options:
 Your selection: """
 welcome = "Welcome to the programming diary!"
 
-entries = [
-    {"content": "Today I started learning programming.", "date": "01-01-2024"},
-    {"content": "I created my first SQLite database!", "date": "02-01-2024"},
-    {"content": "I finished writing my programming diary application.", "date": "03-01-2024"},
-    {"content": "Today I'm going to continue learning programming!", "date": "04-01-2024"},
-]
+def prompt_new_entry():
+    entry_content = input("What have you learned today? ")
+    entry_date = input("Enter the date: ")
+
+    add_entry(entry_content, entry_date)
+    
+def view_entries(entries):
+    for entry in entries:
+        print(f"{entry['date']}\n{entry['content']}\n\n")
+    
 
 print(welcome)
 
 while (user_input := input(menu)) != "3":
     if user_input == "1":
-        entry_content = input("What have you learned today? ")
-        entry_date = input("Enter the date: ")
-
-        add_entry(entry_content, entry_date)
+        prompt_new_entry()
     elif user_input == "2":
-        entries = get_entries()
+        view_entries(get_entries())
 
-        for entry in entries:
-            print(f"{entry['date']}\n{entry['content']}\n\n")
     else:
         print("Invalid option, please try again!")
 
