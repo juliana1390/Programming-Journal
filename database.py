4d4087ea09c8ef54ecf2be4df2e1bc4e27ab971b
juliana1390 <juliana13290@gmail.com>
2024-01-27 15:31:26 -0300

    add_entry function update (insert into table)

--- database.py
+++ database.py
@@ -3,10 +3,15 @@ connection = sqlite3.connect("data.db")
 
 def create_table():
     with connection:
-        connection.execute("CREATE TABLE entries (content TEXT, date TEXT);")
+        connection.execute(
+            "CREATE TABLE IF NOT EXISTS entries (content TEXT, date TEXT);"
+            )
     
 def add_entry(entry_content, entry_date):
-    entries.append({"content": entry_content, "date": entry_date})
+    with connection:
+        connection.execute(
+            "INSERT INTO entries VALUES (?, ?);", (entry_content, entry_date) # Avoids SQL injection attack
+            )
 
 
 def get_entries():
